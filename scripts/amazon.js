66528edfb10b1dcbefc1b4ed13fc03eca766d9d5
Anku009 <aharsh039@gmail.com>
2025-05-19 12:13:23 -0700

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,45 +1,10 @@
-const products = [{
-   Image:' images/products/athletic-cotton-socks-6-pairs.jpg',
-   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-   rating:{
-    stars: 4.5,
-    count : 87
-   },
-   price:1090
-},{
-    Image : 'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    rating:{
-        stars: 4,
-        count:127
-    },
-    price:2095
-},{
-    Image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars: 4.5,
-        count:56
-    },
-    price:799
-},{
-    Image:'images/products/black-2-slot-toaster.jpg',
-    name: '2 Slot Toaster - Black',
-    rating:{
-        stars:5.0,
-        count:2197
-    },
-    price:1899
-},{
-    Image
-}];
 let productsHTML= ''
 products.forEach((products) =>{
    productsHTML += `
       <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${products.Image}">
+              src="${products.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
